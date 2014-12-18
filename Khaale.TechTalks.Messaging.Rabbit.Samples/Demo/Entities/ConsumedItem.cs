dd3e41d157072f39394e9965f290bf1f4f089d42
khaale <khaale@yandex.ru>
2014-12-19 00:01:20 +0400

    [+] SqlCE/SQLCompact support.

--- Khaale.TechTalks.Messaging.Rabbit.Samples/Demo/Entities/ConsumedItem.cs
+++ Khaale.TechTalks.Messaging.Rabbit.Samples/Demo/Entities/ConsumedItem.cs
@@ -1,7 +1,15 @@
-﻿namespace Khaale.TechTalks.Messaging.Rabbit.Samples.Demo.Entities
+﻿using System;
+
+namespace Khaale.TechTalks.Messaging.Rabbit.Samples.Demo.Entities
 {
 	public class ConsumedItem
 	{
+		public ConsumedItem()
+		{
+			CreatedOn = DateTime.UtcNow;
+		}
+
 		public int Id { get; set; }
+		public DateTime CreatedOn { get; set; }
 	}
 }
